daa77b2fc8d44eb169a5960476acd2ef35599924
titova <meilutitova@gmail.com>
2024-10-08 17:54:07 +0700

    java HelpClassDemo

--- help.java
+++ help.java
@@ -1,43 +1,76 @@
-class Help {
-        public static void main(String args[]) throws java.io.IOException {
-                char choice, ignore;
-                do {
-                        System.out.println("Справка по операторам: ");
-                        System.out.println(" 1. if ");
-                        System.out.println(" 2. switch ");
-                        System.out.println(" 3. for");
-                        System.out.println(" 4. while");
-                        System.out.println(" 5. do-while");
-                        System.out.println(" Выберите номер раздела: ");
-                        choice = (char) System.in.read();
-                        do{
-                                ignore = (char) System.in.read();
-                        } while (ignore != '\n');
-                } while (choice < '1' | choice > '5');
-
-
-                System.out.println("\n");
-                switch(choice) {
-                        case '1':
-                                System.out.println("Оператор if:\n");
-                                System.out.println("if(условие) оператор;");
-                                System.out.println("else оператор;");
-                                break;
-                        case '2':
-                                System.out.println("Оператор switch:\n");
-                                System.out.println("switch(выражение) {");
-                                System.out.println(" case константа:");
-                                System.out.println("  последовательность операторов");
-                                System.out.println("  break;");
-                                System.out.println(" ... ");
-                                System.out.println("} ");
-break;                        case '3':
-                                System.out.println("Цикл for:\n");                                System.out.println("for (инициализация; условие; интерация)");
-                                System.out.println("  оператор;");                                break;
-                        case '4':                                System.out.println("Цикл while:\n");
-                                System.out.println("while (условие) оператор;");                                break;
-                        case '5':                                System.out.println("Цикл do-while:\n");
-                                System.out.println("do {");                                System.out.println("  оператор");
-                                System.out.println("while (условие);");                                break;
-                }        }
+class help {
+	void helpOn(int what){
+		switch(what) {
+			case '1':
+				System.out.println("Оператор if :\n");
+				System.out.println("if(условие) оператор;");
+				System.out.println("else оператор;");
+				break;
+			case '2':
+				System.out.println("Оператор swith:\n");
+				System.out.println("switch(выражение) {");
+				System.out.println("case константа:");
+				System.out.println("последовательность операторов");
+				System.out.println("	break");
+				System.out.println(" ... ");
+				System.out.println("} ");
+				break;
+			case '3':
+				System.out.println("Цикл for:\n");
+				System.out.println("for (инициализация; yslovie;enercheya");
+				System.out.println(" оператор;");
+				break;
+			case '4':
+				System.out.println("Chikl while:\n");
+				System.out.println("while (условие) оператор;");
+				break;
+			case '5':
+				System.out.println("Chikl do-while:\n");
+				System.out.println("do{'");
+				System.out.println("оператор;");
+				System.out.println("while (yslovie);");
+				break;
+			case '6':
+				System.out.println("Оператор break:\n");
+				System.out.println("break; ili break metka;");
+			case '7':
+				System.out.println("Оператор break:\n");
+				System.out.println("continue; ili continue metka");
+				break;
+		}
+		System.out.println("\n");
+	}
+	void showMenu() {
+			System.out.println("Справка по операторам:");
+			System.out.println("1. if");
+			System.out.println("2. switch");
+			System.out.println("3. for");
+			System.out.println("4. while");
+			System.out.println("5. do-while");
+			System.out.println("6. break");
+			System.out.println("7. continue");
+			System.out.println("Выберите номер раздела:(для завершения программы нажмите q):");
+	}
+	boolean isValid(int ch) {
+		if (ch < '1' | ch > '7' & ch!= 'q')
+			return false;
+		else
+			return true;
+	}
+}
+class HelpClassDemo {
+	public static void main(String args[]) throws java.to.IOException {
+		char choice, ignore;
+		help hlpobj=new help();
+		for(;;){
+			do {
+				hlpobj.showMenu();
+				choice = (char) System.in.read();
+				do {
+				ignore = (char) System.in.read();
+			} while (!hlpobj.isVaid(choice));
+			if(choice == 'q') break;
+			System.out.println("\n");
+			hlpobj.helpOn(choice);
+		}
 }
